5a8d44d83825434c78302d511bc9cc519387ae5c
young_tea <3h.young.tea@gmail.com>
2026-03-24 07:03:30 +0800

    Refactor gen.cpp for improved readability and structure

--- gen.cpp
+++ gen.cpp
@@ -1,16 +1,18 @@
-#include<bits/stdc++.h>
-#include"fusulib.h"
-using namespace std::string_literals;
-using lf=double;
-using ll=std::ptrdiff_t;
-using ull=std::size_t;
-using us=unsigned;
-auto	nxt_lf(void)->lf{lf x;std::cin>>x;return x;}
-auto	nxt_ll(void)->ll{ll x;std::cin>>x;return x;}
-auto	nxt_ull(void)->ull{ull x;std::cin>>x;return x;}
-auto	nxt_us(void)->us{us x;std::cin>>x;return x;}
-constexpr ll inf=0x3f3f3f3f3f3f3f3fl;
-constexpr ll mod=0x3b800001l;
+#include <bits/stdc++.h>
+#include "fusulib.hpp"
+
+using std::complex_literals::operator""i;
+using std::string_literals::operator""s;
+
+constexpr long k_inf = 0x3f3f3f3f3f3f3f3fl;
+constexpr long k_mod = 0x3b800001l;
+
+auto	nxt(void) {
+	long x;
+	std::cin >> x;
+	return x;
+}
+
 /*
 prob link:	https://vjudge.net/problem/QOJ-10148
 start think:	
@@ -18,60 +20,85 @@ term think:
 start code:	
 term code:	
 */
-void	make_data(ll t){
-	fusu::fout<<t<<" 1\n"s;
-	ll n=100000,m=200000,q=100000;
-	fusu::fout<<n<<" "s<<m<<" "s<<q<<"\n"s;
-	while(m--){
-		auto [x,y]=fusu::next_range(1l,n);
-		while(x==y)
-			std::tie(x,y)=fusu::next_range(1l,n);
-		fusu::fout<<x<<" "s<<y<<"\n"s;
-	}	ll *a=new ll[n+1]();
-	for(ll x=1;x<=n;x++)
-		a[x]=x;
-	std::shuffle(a+1,a+1+n,fusu::hack);
-	for(ll x=1;x<=n;x++)
-		fusu::fout<<a[x]<<" \n"s[x==n];
-	std::shuffle(a+1,a+1+n,fusu::hack);
-	for(ll x=1;x<=n;x++)
-		fusu::fout<<a[x]<<" \n"s[x==n];
-	delete[] a;
-	while(q--){
-		ll op=fusu::next_x(1,3);
-		fusu::fout<<op<<" "s;
-		if(op<=2){
-			auto [x,y]=fusu::next_range(1l,n);
-			fusu::fout<<x<<" "s<<y<<"\n"s;
-		}else{	ll x=fusu::next_x(1l,n);
-			auto [f,g]=fusu::next_range(1l,n);
-			fusu::fout<<x<<" "s<<f<<" "s<<g<<"\n"s;
+
+void	make_data(long task) {
+	long vtxes = 1e5, edges = 2e5, queries = 1e5;
+	
+	std::println(fs::fout, "{} 1", task);
+	std::println(fs::fout, "{} {} {}", vtxes, edges, queries);
+
+	while (edges--) {
+		auto [x, y] = fs::nxt_tied(1, vtxes);
+
+		while (x == y)
+			std::tie(x, y) = fs::nxt_tied(1, vtxes);
+
+		std::println(fs::fout, "{} {}", x, y);
+	}
+
+	std::vector<long> a(vtxes);
+
+	for (long x : std::views::iota(0, vtxes))
+		a[x] = x + 1;
+
+	std::shuffle(a.begin(), a.end(), fs::hack);
+
+	for (long x : std::views::iota(0, vtxes))
+		std::print(fs::fout, "{}{}", a[x], " \n"s[x == vtxes - 1]);
+
+	std::shuffle(a.begin(), a.end(), fs::hack);
+
+	for (long x : std::views::iota(0, vtxes))
+		std::print(fs::fout, "{}{}", a[x], " \n"s[x == vtxes - 1]);
+
+	while (queries--) {
+		long op = fs::nxt_x(1, 3);
+
+		if (op == 1 || op == 2) {
+			auto [x, y] = fs::nxt_tied(1, vtxes);
+
+			while (x == y)
+				std::tie(x, y) = fs::nxt_tied(1, vtxes);
+
+			std::println(fs::fout, "{} {} {}", op, x, y);
+		} else if (op == 3) {
+			long x = fs::nxt_x(1, vtxes);
+			auto [f, g] = fs::nxt_tied(1, vtxes);
+			std::println(fs::fout, "{} {} {} {}", op, x, f, g);
+		} else {
+			std::println(std::cerr, "i`ve aked wf!");
 		}
 	}
 }
-void	solve(void){
-	std::string prob="recall"s;
-	ll tot_case=25;
-	std::string stdx="std"s;
-	std::string folder="recall"s;
-	std::string path="./"s+folder+"/"s;
-	fusu::mkdir(path);
-	fusu::set_seed(std::chrono::system_clock::now().time_since_epoch().count());
-	for(ll t=1;t<=tot_case;t++){
-		std::string task=prob+std::to_string(t);
-		fusu::gen_in(path+task+".in"s,[t]{make_data(t);});
-		fusu::gen_ans(path+task,stdx);
+
+void	solve(void) {
+	std::string prob = "recall"s;
+	long cases_tot = 25;
+	std::string path = "./"s + prob + "/"s;
+
+	fs::set_seed(std::chrono::system_clock::now().time_since_epoch().count());
+	fs::mkdir(path);
+
+	for (long t : std::views::iota(1, cases_tot + 1)) {
+		fs::gen_in(path + prob, t, make_data);
+		fs::gen_ans(path + prob, t, "std.x"s);
 	}
 }
-auto	main(void)->signed{
-//	freopen(".in","r",stdin);
-//	freopen(".out","w",stdout);
+
+signed	main(void) {
+//	std::freopen(".in", "r", stdin);
+//	std::freopen(".out", "w", stdout);
 	std::ios::sync_with_stdio(0);
 	std::cin.tie(nullptr);
 	std::cout.tie(nullptr);
-	ll _=1;
-//	_=nxt_ll();
-	while(_--)
+
+	long _ = 1;
+
+//	_ = nxt();
+
+	while (_--)
 		solve();
-	return 0;
+
+	std::cout.flush();
+	return EXIT_SUCCESS;
 }
